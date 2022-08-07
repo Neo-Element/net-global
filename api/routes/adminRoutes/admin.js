728cfaf166e88cae4f79e3a3e1c3bcc09d313e05
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2022-08-07 13:05:25 -0300

    changes and checks routes for admins

--- api/routes/adminRoutes/admin.js
+++ api/routes/adminRoutes/admin.js
@@ -1,10 +1,10 @@
 const {AdminController}= require('../../controllers/adminController')
 const router= require('express').Router()
 
-router.get("/disabled/all", AdminController.getAllDisabled);
-router.get("/disabled", AdminController.getAdminsDisabled);
-router.post("/disabled/:id", AdminController.disabledAdmins);
-router.put("/request/absence/:id", AdminController.responseToRequest);
-router.put("/rehabited/:id", AdminController.rehabitedAdmins);
+router.get("/disabled/all", AdminController.getAllDisabled);//ready
+router.get("/disabled", AdminController.getAdminsDisabled);//ready
+router.post("/disabled/:id", AdminController.disabledAdmins);//ready
+router.put("/request/absence/:id", AdminController.responseToRequest);// await security services ready for testing
+router.put("/rehabited/:id", AdminController.rehabitedAdmins);// ready
 
 module.exports= router
