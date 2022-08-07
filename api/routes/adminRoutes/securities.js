b83661fed8b89694380fb0ce8956df6eeaf63330
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2022-08-06 21:34:01 -0300

    changes and delete some routes in relation update for securities services

--- api/routes/adminRoutes/securities.js
+++ api/routes/adminRoutes/securities.js
@@ -2,21 +2,21 @@ const adminRouter = require("express").Router()
 const{SecurityController}= require('../../controllers/adminController')
 
 adminRouter.get("/", SecurityController.getAllSecurities);// ready
-adminRouter.get("/:search", SecurityController.SearchOneSecurity);// ready
+adminRouter.get("/disabled",SecurityController.getSecuritiesDisabled);//ready
+adminRouter.get("/id/:id", SecurityController.SearchOneSecurityId);// ready
+adminRouter.get("/cuil/:cuil", SecurityController.SearchOneSecurityCuil)//ready
 adminRouter.get("/name/:name", SecurityController.getOneSecurity)//ready
 adminRouter.get("/office/:name",SecurityController.getAllSecuritiesByOffice);//ready
 adminRouter.get("/securitiesByDistance/:id",SecurityController.getSecuritiesByDistance);
 adminRouter.get("/provincie/:name",SecurityController.getSecuritiesByProvincie);//ready
 adminRouter.get("/images/day/:id", SecurityController.getImageSecurityByDay);//no testeable yet
-adminRouter.get("/disabled",SecurityController.getSecuritiesDisabled);
-adminRouter.get("/all/request", SecurityController.getAllRequest);
-adminRouter.get("/oneResquest/:id", SecurityController.getOneRequest);
-adminRouter.post("/add", SecurityController.addSecurity);
-adminRouter.post("/add/provincie",SecurityController.addSecurityProvincie);
-adminRouter.post("/disabled/:id",SecurityController.disabledSecurity);
-adminRouter.put("/rehabited/:id",SecurityController.rehabitedSecurities);
-adminRouter.put("/edit/:id", SecurityController.editSecurity);
-adminRouter.put("/edit/status/:id",SecurityController.editSecurityStatus);
-adminRouter.patch("/changePassword/:id", SecurityController.patchPassword);
+adminRouter.get("/all/request", SecurityController.getAllRequest);// teateable with routes of PWA
+adminRouter.get("/oneResquest/:id", SecurityController.getOneRequest);//teasteable with routes of PWA
+adminRouter.post("/add", SecurityController.addSecurity);//ready
+adminRouter.post("/add/provincie",SecurityController.addSecurityProvincie);//ready
+adminRouter.post("/disabled/:id",SecurityController.disabledSecurity);//ready
+adminRouter.put("/rehabited/:id",SecurityController.rehabitedSecurities);// ready
+adminRouter.put("/edit/:id", SecurityController.editSecurity);//ready
+
 
 module.exports = adminRouter;
