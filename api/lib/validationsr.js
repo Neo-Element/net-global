2143095617825ae9c35e17f090c89eefdec10aca
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2022-08-15 20:08:14 -0300

    update logic for calendar assignament

--- api/lib/validationsr.js
+++ api/lib/validationsr.js
@@ -10,9 +10,13 @@ const validateCreateWorkDay = async (
   let listHoures = [];
   const entryHour = Number(inHour.split(":")[0]);
   const closingHour = Number(exitHour.split(":")[0]);
-
-  for (let i = entryHour; i <= closingHour; i++) {
-    listHoures.push(i);
+  let contador1= entryHour
+  while (contador1 !== closingHour) {
+    if (contador1 === 23) listHoures.push(contador1);
+    if(contador1 === entryHour) listHoures.push(contador1)
+    let hour = transcriptorHour(contador1, closingHour);
+    listHoures.push(hour);
+    contador1 = hour;
   }
   console.log("hours first", listHoures);
   let listNewHour = [];
@@ -22,6 +26,7 @@ const validateCreateWorkDay = async (
   let contador = newEntryHour;
   while (contador !== newClosingHour) {
     if (contador === 23) listNewHour.push(contador);
+    if(contador === newEntryHour) listNewHour.push(contador)
     let hour = transcriptorHour(contador, newClosingHour);
     listNewHour.push(hour);
     contador = hour;
@@ -42,6 +47,9 @@ function transcriptorHour(entry, closing) {
   if (entry === 24) return 0;
   if (entry !== 24) return (entry = entry + 1);
 }
+
+
+
 
 function enableOrDisable(instance) {
   instance.status = !instance.status;
